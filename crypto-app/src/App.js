b07228cc84e57a06cf4948aec1509d2c31e0d477
Priyank Gajera <priyankgajera67@hotmail.com>
2020-10-18 21:02:55 -0400

    Remove unused code.

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 var ccxt = require ('ccxt')
 
 function App() {
-  var test = new ccxt.kraken (); // print all available exchanges
-  console.log( test.loadMarkets () );
   return (
     <div className="App">
       <header className="App-header"></header>
